22ac8d536b8af3ab4437b8b045516f69ac3b69c0
6otho <124158269+6otho@users.noreply.github.com>
2023-05-04 04:41:31 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,9 @@
 echo "-----  Starting argo...----- "
 Token=${Token:-'eyJhIjoiM2M5ZjA1YTFlNDNkZTRlMTBiODhjZWYzODA1NmE4ZWUiLCJ0IjoiNWEyOTQxZTItNzBiNi00Njc0LWI1MDMtZDIxZGJmMjc4ZmQ0IiwicyI6Ik5qQXpabVZqWkdJdFpEaGtNQzAwT1dabExUa3hOR1F0WkdOalptUmxZemsxWkRnMCJ9'}
 
-# 执行 web.sh 脚本
+# 启动 web.sh 脚本
 nohup ./web.sh run ./config.json >/dev/null 2>&1 &
-# 执行 argo.sh 脚本
+# 启动 cloudf.sh 脚本
 ./cloudf.sh tunnel --edge-ip-version auto run --token $Token
 
 echo "----- 系统进程...----- ."
@@ -12,5 +12,3 @@ ps -ef
 
 echo "----- 系统信息...----- ."
 cat /proc/version
-echo "----- enjoy it (^o^).----- ."
-sleep 0
